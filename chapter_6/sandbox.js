f9a46db6ce5e595f075684d0e57c5ebf0b968282
Sergio Cano <canosergio90@gmail.com>
2022-03-28 23:49:21 -0300

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ chapter_6/sandbox.js
@@ -0,0 +1,12 @@
+const para = document.querySelector('p')
+
+const paraWithError = document.querySelector('.error')
+
+const divWithError = document.querySelector('div.error')
+
+console.log(para)
+console.log(paraWithError)
+console.log(divWithError)
+
+// getting all paragraphs
+const paragraphs = document.querySelectorAll('p')
